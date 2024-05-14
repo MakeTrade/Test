376fffd31498832c772468f74f358a53eccd6b07
MakeTrade <44473011+MakeTrade@users.noreply.github.com>
2024-05-14 17:44:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
  const TOKEN='6959741919:AAG4qC7-n8wuTdPwxoQ_B0m0iBHvwP-VM9I'
-const SOLANA_RPC_URL='https://old-holy-hill.solana-devnet.quiknode.pro/68e437c2ab47fce951bc8be36587ea46d0fd510b/'
+const SOLANA_RPC_URL='https://go.getblock.io/151f73be2f4146cc842f0cf0f8ae46b0'
 
  module.exports = {TOKEN,SOLANA_RPC_URL}
